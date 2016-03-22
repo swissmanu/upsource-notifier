906b9c1b5f04e7d7e5517170405092d4aebff9ce
Manuel Alabor <manuel@alabor.me>
2016-03-22 07:52:59 +0100

    more restructuring

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 // curl -X POST -u mal: --cert "Manuel Alabor" -H "Content-Type: application/json" -d '{"limit": 1, "project": "jaso-backoffice-ui", "type": 1}' https://review.scaling.ch/~rpc/getFeed
 // https://review.scaling.ch/~api_doc/reference/Projects.html#messages.FeedRequestDTO
 
-const notifier = require('node-notifier');
-const path = require('path')
-
 const config = require('./config.json')
 const Connection = require('./lib/connection')
-const AvatarService = require('./lib/avatarService')
+const FeedEventHandler = require('./lib/feedEventHandler')
+const AvatarCache = require('./lib/avatarCache')
 
 const connection = new Connection(config)
 connection.connect()
   .then((connection) => {
-    return connection.rpc.getCurrentUser()
+    const avatarCache = new AvatarCache(connection)
+    new FeedEventHandler(connection, avatarCache)
   })
-  .then((currentUser) => {
-    return connection.rpc.getUserInfo(currentUser.userId)
-  })
-  .then((userInfo) => {
-    console.log(userInfo)
-  })
+  .catch((err) => console.error(err))
 
 // var clientCertificate = fs.readFileSync(config.clientCertificate)
 // https.globalAgent.options.pfx = clientCertificate
