8adc3c511b011b2d6fc0883b5b70e07ea2a62378
Manuel Alabor <manuel@alabor.me>
2016-03-22 08:47:38 +0100

    feed events implemented and added debug support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // curl -X POST -u mal: --cert "Manuel Alabor" -H "Content-Type: application/json" -d '{"limit": 1, "project": "jaso-backoffice-ui", "type": 1}' https://review.scaling.ch/~rpc/getFeed
 // https://review.scaling.ch/~api_doc/reference/Projects.html#messages.FeedRequestDTO
 
+const debug = require('debug')('upsource-notifier')
 const config = require('./config.json')
 const Connection = require('./lib/connection')
 const FeedEventHandler = require('./lib/feedEventHandler')
@@ -9,10 +10,11 @@ const AvatarCache = require('./lib/avatarCache')
 const connection = new Connection(config)
 connection.connect()
   .then((connection) => {
+    debug('Connected')
     const avatarCache = new AvatarCache(connection)
     new FeedEventHandler(connection, avatarCache)
   })
-  .catch((err) => console.error(err))
+  .catch((err) => debug(`Error ${JSON.stringify(err, null, 2)}`))
 
 // var clientCertificate = fs.readFileSync(config.clientCertificate)
 // https.globalAgent.options.pfx = clientCertificate
