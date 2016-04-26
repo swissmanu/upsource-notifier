6ec727b2f4f946472775ff5df45e75164294869e
Manuel Alabor <swissmanu@users.noreply.github.com>
2016-04-26 14:09:16 +0200

    chore(index): Remove leftover comment

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// curl -X POST -u mal: --cert "Manuel Alabor" -H "Content-Type: application/json" -d '{"limit": 1, "project": "jaso-backoffice-ui", "type": 1}' https://review.scaling.ch/~rpc/getFeed
-// https://review.scaling.ch/~api_doc/reference/Projects.html#messages.FeedRequestDTO
-
 const debug = require('debug')('upsource-notifier')
 const Connection = require('./lib/connection')
 const FeedEventHandler = require('./lib/feedEventHandler')
